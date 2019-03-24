4ed1f2f1116e33ccae1ad65e1724a9aebbb852ed
crotoc <crotoc@gmail.com>
2019-03-24 13:55:53 -0500

    Fix a bug in conditions.

--- luci-app-v2ray-pro/root/etc/v2ray/up-unblock-youku.sh
+++ luci-app-v2ray-pro/root/etc/v2ray/up-unblock-youku.sh
@@ -4,7 +4,7 @@ wget-ssl --no-check-certificate https://raw.githubusercontent.com/crotoc/unblock
 
 cat /tmp/ol-unblock-youku.b64 | base64 -d > /tmp/ol-unblock-youku.txt
 
-if [ -s "/tmp/myunblock.txt" ];then
+if [ -s "/tmp/ol-unblock-youku.txt" ];then
 	sort -u /etc/v2ray/base-ublist.txt  /tmp/ol-unblock-youku.txt | uniq > /tmp/unblock-youku
 	if ( ! cmp -s /tmp/unblock-youku /etc/gfwlist/unblock-youku );then
 		if [ -s "/tmp/unblock-youku" ];then
@@ -17,7 +17,7 @@ if [ -s "/tmp/myunblock.txt" ];then
 fi
 
 rm -f /tmp/ol-unblock-youku.b64
-rm -f /tmp/ol-unblock-youku.txt
-rm -f /tmp/unblock-youku
+#rm -f /tmp/ol-unblock-youku.txt
+#rm -f /tmp/unblock-youku
 
 /etc/init.d/v2raypro restart
